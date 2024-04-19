2276e91154933bf17921b701b7d848f5886429cf
baksabence <gxdcloud@gmail.com>
2024-04-19 10:17:38 +0200

    submit felkesz (a nem atirodik ujabb hozzaadasnal)

--- urlapKezelo.js
+++ urlapKezelo.js
@@ -2,12 +2,16 @@
 // beletesszük a listába
 import { init } from "./main.js";
 
+
+
 export function ujAdat(lista){
 
     const nevElem = $("#nev")
     const korElem = $("#kor")
-    const nemElem = $("input[name='nem']:checked")
+    
+    
     const submitElem = $("#submit")
+
     const adatOBJ = {
         nev: "",
         kor: 0,
@@ -15,12 +19,13 @@ export function ujAdat(lista){
 
     }
     submitElem.on("click", function(event){
+        const nemElem = $("input[name='nem']:checked")
         event.preventDefault(); // leszedi az alapértelmezett hozzárendelt eventet
         adatOBJ.nev = nevElem.val()
         adatOBJ.kor = korElem.val()
-        adatOBJ.nem = (nemElem.val() === "true" ? true : false)
+        adatOBJ.nem = (nemElem.val() === "false" ? false : true)
         console.log(adatOBJ);
-
+        
         //sumbit ellenorzese    
         const validELEM = $(".valid-feedback")
         const nevVALIDE = validELEM.eq(0).css("display") !== "none"
@@ -32,8 +37,6 @@ export function ujAdat(lista){
         }else{
             console.log("Hibás adatok!")
         }
-
-        lista.push(adatOBJ);
         init(lista);
     })
 
